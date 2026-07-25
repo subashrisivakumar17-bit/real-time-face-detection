6e7492e0a2feff76ae4f41a8785d0ddcf4668732
subashrisivakumar17-bit <subashrisivakumar17@gmail.com>
2026-07-25 22:58:23 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import cv2
 import av
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
 st.set_page_config(page_title="Real Time Face Detection")
 
 st.title("📷 Real Time Face Detection")
 
-# Load Haar Cascade
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
@@ -42,7 +41,7 @@ class FaceDetector(VideoProcessorBase):
             cv2.FONT_HERSHEY_SIMPLEX,
             0.8,
             color,
-            2
+            2,
         )
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
@@ -50,17 +49,23 @@ class FaceDetector(VideoProcessorBase):
 
 webrtc_streamer(
     key="face-detection",
-    mode=WebRtcMode.SENDRECV,
     video_processor_factory=FaceDetector,
-    media_stream_constraints={
-        "video": True,
-        "audio": False,
-    },
+    media_stream_constraints={"video": True, "audio": False},
     rtc_configuration={
         "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]},
-            {"urls": ["stun:stun1.l.google.com:19302"]},
-            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {
+                "urls": ["stun:stun.relay.metered.ca:80"],
+            },
+            {
+                "urls": [
+                    "turn:global.relay.metered.ca:80",
+                    "turn:global.relay.metered.ca:80?transport=tcp",
+                    "turn:global.relay.metered.ca:443",
+                    "turns:global.relay.metered.ca:443?transport=tcp",
+                ],
+                "username": "1b0b30ea045d4345a94c32c3",
+                "credential": "trpj8BygLro+bnDt",
+            },
         ]
     },
 )
